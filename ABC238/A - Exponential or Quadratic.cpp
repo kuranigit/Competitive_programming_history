f91c2ec8961f04a47bb390293f42b1b9ac91d33a
kuranigit <82947479+kuranigit@users.noreply.github.com>
2022-09-15 07:23:32 +0900

    誤って削除したものを復元

---
+++ ABC238/A - Exponential or Quadratic.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if (n > 4) {
+        cout << "Yes";
+    }
+    else {
+        if (n == 1) {
+            cout << "Yes";
+        }
+        else {
+            cout << "No";
+        }
+    }
+}
